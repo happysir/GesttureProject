0d7a5f86954c7d1395f5b35c204d8b81a861535d
jm7___ <meiqi-8710@163.com>
2013-03-19 22:44:22 +0800

    Create mainactivity.java

---
+++ mainactivity.java
@@ -0,0 +1,8 @@
+public class MainActivity extends Activity {
+  private GestureOverlayView gov;
+  private Gesture gesture;
+  private GestureLibrary gestureLib;
+  private TextView tv;
+  private EditText et;
+  private String path;
+  private File file; 
